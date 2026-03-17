984d9102a26655393376eb6e383357b426820cdd
Taina Adrielly] <martins.taina@aluno.ifsp.edu.br>
2026-03-17 08:28:32 -0300

    aula01

--- loja/src/components/Topo/index.jsx
+++ loja/src/components/Topo/index.jsx
@@ -4,21 +4,23 @@ export default function Topo() {
     return (
         <header>
             <div className='anuncio'>
-                <p>Tudo em 10x sem juros!</p>
+                <div className='anuncio-conteudo'>
+                    <p>Tudo em 10x sem juros!</p>
+                </div>
             </div>
 
             <div className='topo'>
                 <picture>
                     <a href="#">
-                        <img src='./logoLoja.png'/>
+                        <img src='./logoLoja.png' />
                     </a>
                 </picture>
 
                 <nav>
                     <a href='#'>Home</a>
-                     <a href='#'>Produtos</a>
-                      <a href='#'>Lançamentos</a>
-                       <a href='#'>Contatos</a>
+                    <a href='#'>Produtos</a>
+                    <a href='#'>Lançamentos</a>
+                    <a href='#'>Contatos</a>
                 </nav>
             </div>
         </header>
